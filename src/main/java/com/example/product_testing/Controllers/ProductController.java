533a0a311361231e5cebee8c9a3b825c32ed3a4a
Muhodari Sage <sagemuho@gmail.com>
2021-11-25 21:39:11 +0200

    finishes with crud application

--- src/main/java/com/example/product_testing/Controllers/ProductController.java
+++ src/main/java/com/example/product_testing/Controllers/ProductController.java
@@ -3,15 +3,15 @@ package com.example.product_testing.Controllers;
 
 import com.example.product_testing.Repository.ProductRepository;
 import com.example.product_testing.Services.ProductServices;
+import com.example.product_testing.dto.UpdateProductDto;
 import com.example.product_testing.models.Product;
 import com.example.product_testing.utils.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -43,7 +43,24 @@ return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(false,"
 }
 
 
+@PostMapping("/add-new-product")
+    public  ResponseEntity<?> saveItem(@Valid Product product){
+       
+       if(productRepository.existsByName(product.getName())){
+           return  ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                   .body(new APIResponse(false,"Product name already exixt"));
+       }
+       productRepository.save(product);
 
+       return  ResponseEntity.ok(product)
+}
+
+
+@PutMapping
+    public  ResponseEntity<?> updateProduct(@PathVariable(name="id")int id, @Valid UpdateProductDto dto){
+       return  productServices.updateProduct(id,dto);
+
+}
 
 
 }
