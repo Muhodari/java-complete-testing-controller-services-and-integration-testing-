968ba3caa94dbed4dd1e49a5167d588a240c16c7
Muhodari Sage <sagemuho@gmail.com>
2021-11-26 08:23:21 +0200

    added updateAll_success service  teste

--- src/test/java/com/example/product_testing/service/ProductServiceTest.java
+++ src/test/java/com/example/product_testing/service/ProductServiceTest.java
@@ -2,16 +2,20 @@ package com.example.product_testing.service;
 
 import com.example.product_testing.Repository.ProductRepository;
 import com.example.product_testing.Services.ProductServices;
+import com.example.product_testing.dto.UpdateProductDto;
 import com.example.product_testing.models.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -31,6 +35,21 @@ public class ProductServiceTest {
           ));
   assertEquals(10,productServices.getAll().get(0).getValue() );
  }
+
+
+ @Test
+    public  void updateAll_success(){
+     UpdateProductDto dto= new UpdateProductDto("money",3,4,"let update new product");
+     Product product = new Product(1,"money",54,5,"let update data given");
+     when(productRepositoryMock.findById(1)).thenReturn(Optional.of(product));
+     when(productRepositoryMock.existsByName(dto.getName())).thenReturn(true);
+     when(productRepositoryMock.save(product)).thenReturn(product);
+
+     ResponseEntity<?> updateProduct=  productServices.updateProduct(1,dto);
+     assertTrue(updateProduct.getStatusCode().is2xxSuccessful());
+
+ }
+
 
 
 
